012bbcad759559bc6fd3fe93c2abb2550743a16e
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-30 22:48:07 +0530

    Completed Basic Setup and App.js

--- cryptobyte/src/App.js
+++ cryptobyte/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Navbar from "./Navbar"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Marketplace from "./Marketplace"
+import Buy_sell from "./Buy_sell"
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    Project Initialized
+    <Navbar/>
+    <Switch>
+      <Route path="/Marketplace">
+        <Marketplace/>
+      </Route>
+      <Route path="/buy_sell">
+        <Buy_sell/>
+      </Route>
+
+    </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ cryptobyte/src/Buy_sell.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Buy_sell() {
+    return (
+        <div>
+            This is Buy and Sell
+        </div>
+    )
+}
+
+export default Buy_sell
