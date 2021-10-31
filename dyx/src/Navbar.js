9170c1e69978932d7379c9e9a1fcf843fd3fed8a
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-31 12:08:51 +0530

    Navbar Created Version 2

---
+++ dyx/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <div className="navbar">
+        <div className="options">
+        <h3 className="h31">About</h3>
+        <h3 className="h32">Sign In / Sign Up</h3>
+        </div>
+        </div>
+    )
+}
+
+export default Navbar
